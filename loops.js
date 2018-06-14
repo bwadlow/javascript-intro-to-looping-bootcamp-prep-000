25624058a188fcab44873e1a00ebd83d425e7f37
Brian Wadlow <bwadlow@gmail.com>
2018-06-14 22:38:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 function forLoop(arr) {
   for (let i = 0; i < arr.length; i++) {
-   return i===1 ? arr.push(`I am ${i} strange loop`) : arr.push(`I am ${i} strange loops`)
+   i===1 ? arr.push(`I am ${i} strange loop`) : arr.push(`I am ${i} strange loops`)
   }
+  return arr
  }
